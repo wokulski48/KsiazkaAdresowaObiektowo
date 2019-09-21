794fd11af06b783bd1fd5a1299ca93ea8101c777
Michal Czopek <michal.czopek.programista@gmail.com>
2019-09-21 22:36:16 +0200

    Add registered user to file

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
+#include <fstream>
+#include <sstream>
 
 #include "Uzytkownik.h"
 
@@ -12,14 +14,21 @@ class KsiazkaAdresowa
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
 
+    string nazwaPlikuZUzytkownikami;
+
     vector <Uzytkownik> uzytkownicy;
 
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
     bool czyIstniejeLogin(string login);
     string wczytajLinie();
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    string konwerjsaIntNaString(int liczba);
+    bool czyPlikJestPusty(fstream &plikTekstowy);
 
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
 
